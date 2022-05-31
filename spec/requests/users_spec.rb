b3cacf89492816cde20b865f0431a26b0399f7ac
yersel500 <yersel500@gmail.com>
2022-05-31 18:00:12 -0500

    test for Posts Controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,20 +18,20 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  describe 'Test Posts Controller' do
-    it 'get users' do
-      get users_path
+  describe 'Test Users Controller with specific id' do
+    it 'get user by id' do
+      get '/users/:id'
       expect(response).to have_http_status(200)
     end
 
-    it 'correct template rendered' do
-      get users_path
-      expect(response).to render_template(:list_of_users)
+    it 'correct template rendered user by id' do
+      get '/users/:id'
+      expect(response).to render_template(:posts_per_user)
     end
 
-    it 'response body include placeholder text' do
-      get users_path
-      expect(response.body).to include('<h1>Here is the list of users!</h1>')
+    it 'response body include placeholder text user by id' do
+      get '/users/:id'
+      expect(response.body).to include('<h1>Here is the lists of post per user</h1>')
     end
   end
 end
