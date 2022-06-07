9d0ba0ba115b0ead35a3d2a6e991e6836a4e9570
yersel500 <yersel500@gmail.com>
2022-06-07 14:31:45 -0500

    delete unnecesary columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_012500) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_07_193018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,8 +32,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_012500) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -43,7 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_012500) do
     t.string "name"
     t.string "photo"
     t.string "bio"
-    t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
