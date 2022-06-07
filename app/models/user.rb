7bcd063218ac42ef55ff0bc36dcc0c80ee2b0bb2
yersel500 <yersel500@gmail.com>
2022-06-07 16:06:44 -0500

    addd queries

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  # scope :three_recent_post, ->(name) { where('name = ?', name) }
+
+  def self.three_recent_post(author)
+    Post.where(user_id: author).order(updated_at: :DESC).limit(3)
+  end
 end
