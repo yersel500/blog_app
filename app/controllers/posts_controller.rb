12d8784328b6f4ab247477ac1ae88229038aeb44
yersel500 <yersel500@gmail.com>
2022-06-07 21:49:36 -0500

    user profile and posts pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = User.find(params[:user_id]).posts
     render 'list_of_posts'
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
     render 'posts_per_user'
   end
 end
