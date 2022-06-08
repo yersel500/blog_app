c6e1275d00ab1fe47214fff36bfc942b5e4ae89e
yersel500 <yersel500@gmail.com>
2022-06-08 11:00:07 -0500

    Add routes for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   root 'users#index'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
+  get '/users/:id/new', to: 'users#new'
+  post '/users/:id/create', to: 'users#create'
   get 'users/:user_id/posts', to: 'posts#index'
   get 'users/:user_id/posts/:id', to: 'posts#show'
+  get 'users/:user_id/posts/:id/new', to: 'posts#new'
+  post 'users/:user_id/posts/:id/create', to: 'posts#create'
 end
