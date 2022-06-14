0bef43267c6b639200fecd4375ef67aa989a42ff
yersel500 <yersel500@gmail.com>
2022-06-14 14:31:15 -0500

    add buttons and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :bio, :email, :password, :current_password)
     end
-end
+  end
 end
